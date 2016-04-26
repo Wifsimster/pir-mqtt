1c5c82e9b33471c60d8b521028e0134773c9c9ff
bot <contact@arca-computing.fr>
2016-04-26 22:22:23 +0200

    Commit

---
+++ init.lua
@@ -0,0 +1,17 @@
+require('config')
+
+print("Setting up WIFI...")
+
+wifi.setmode(wifi.STATION)
+wifi.sta.config(AP,PWD)
+wifi.sta.connect()
+
+tmr.alarm(1, 1000, 1, function() 
+    if wifi.sta.getip()== nil then 
+        print("Waiting...") 
+    else 
+        tmr.stop(1)
+        print("IP is "..wifi.sta.getip())
+        --dofile("main.lua")
+    end 
+end)
---
+++ main.lua
@@ -0,0 +1,37 @@
+require('config')
+
+DATA_PIN = 3 -- GPIO_0
+
+gpio.mode(DATA_PIN, gpio.INPUT)
+
+-- MQTT client
+TOPIC = "/sensors/bureau/pir/data"
+
+-- Init client with keepalive timer 120sec
+m = mqtt.Client(CLIENT_ID, 120, "", "")
+
+function publish()
+    DATA = '{"motion":"'..gpio.read(DATA_PIN)..'"}'
+    m:publish(TOPIC, DATA, 0, 0, function(conn)
+        print(CLIENT_ID.." sending data: "..DATA.." to "..TOPIC)
+    end)
+end
+        
+tmr.alarm(2, 1000, 1, function()
+    tmr.stop(2)
+    print("Connecting to MQTT: "..BROKER_IP..":"..BROKER_PORT.."...")
+    m:connect(BROKER_IP, BROKER_PORT, 0, function(conn)
+        print("Connected to MQTT: "..BROKER_IP..":"..BROKER_PORT.." as "..CLIENT_ID)
+
+        DATA = '{"motion":"'..gpio.read(DATA_PIN)..'"}'
+        
+        -- Publish a message (QoS = 0, retain = 0)
+        m:publish(TOPIC, DATA, 0, 0, function(conn)
+            print(CLIENT_ID.." sending data: "..DATA.." to "..TOPIC)
+        end)
+        
+        gpio.trig(DATA_PIN, 'both', publish)
+    end)
+end)
+
+m:close();
