3348857521a24b6e3215238181c44291f6c91dbe
Wifsimster <wifsimster@gmail.com>
2017-03-06 19:04:56 +0100

    Update init.lua

--- config.lua
+++ config.lua
@@ -6,7 +6,6 @@ PWD = "192Wifsimster!!"
 BROKER_IP = "192.168.0.35"
 BROKER_PORT = 1883
 CLIENT_ID = "ESP8266-"..node.chipid()
-REFRESH_RATE = 30000 -- ms
 
 -- GPIO
 DATA_PIN = 3 -- GPIO_0
--- init.lua
+++ init.lua
@@ -4,33 +4,19 @@ print("Starting ESP"..node.chipid().."...")
 
 wifi.setmode(wifi.STATION)
 wifi.sta.config(AP,PWD)
-wifi.sta.connect()
 
 print('MAC address:', wifi.sta.getmac())
 
-if (wifi.getmode() == wifi.STATION) then
-    local joinCounter = 0
-    local joinMaxAttempts = 5
-    tmr.alarm(0, 3000, 1, function()
-       local ip = wifi.sta.getip()
-       if ip == nil and joinCounter < joinMaxAttempts then
-          print('Connecting to WiFi Access Point ...')
-          joinCounter = joinCounter +1
-       else
-          if joinCounter == joinMaxAttempts then
-             print('Failed to connect to WiFi Access Point.')
-          else
-             print('IP: ',ip)
-             if file.open("main.lua") ~= nil then
-                dofile("main.lua")
-             else 
-              print("main.lua doesn't exist !")
-             end
-          end
-          tmr.stop(0)
-          joinCounter = nil
-          joinMaxAttempts = nil
-          collectgarbage()
-       end
-    end)
-end
+tmr.create():alarm(1000, tmr.ALARM_AUTO, function(cb_timer)
+    if wifi.sta.getip() == nil then
+        print("Waiting for IP address...")
+    else
+        cb_timer:unregister()
+        print("WiFi connection established, IP address: " .. wifi.sta.getip())
+        if file.open("main.lua") ~= nil then
+            dofile("main.lua")
+        else 
+            print("main.lua doesn't exist !")
+        end
+    end
+end)
