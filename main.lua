f72662758a5abc8cb29c917133cf2baadde9630e
Wifsimster <wifsimster@gmail.com>
2017-05-05 15:11:44 +0200

    Add spaces

--- main.lua
+++ main.lua
@@ -9,15 +9,18 @@ val = 1
 m:lwt("/offline", '{"message":"'..CLIENT_ID..'", "topic":"'..TOPIC..'", "ip":"'..ip..'"}', 0, 0)
         
 print("Connecting to MQTT: "..BROKER_IP..":"..BROKER_PORT.."...")
+
 m:connect(BROKER_IP, BROKER_PORT, 0, 1, function(conn)
     print("Connected to MQTT: "..BROKER_IP..":"..BROKER_PORT.." as "..CLIENT_ID)
 
     DATA = '{"mac":"'..wifi.sta.getmac()..'", "ip":"'..ip..'", "online":"true"}'
+        
     m:publish(TOPIC, DATA, 0, 0, function(conn)
         print(CLIENT_ID.." sending online: "..DATA.." to "..TOPIC)
     end)
     
     gpio.trig(DATA_PIN, 'both', publish)
+        
 end)
 
 function publish(timestamp)
